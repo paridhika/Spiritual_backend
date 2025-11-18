e6798bcbd1eaadcae64e93122c50fb42d31a859b
Paridhika Kayal <paridhika@users.noreply.github.com>
2025-11-18 11:39:11 -0500

    Change default port from 8080 to 8000

--- main.py
+++ main.py
@@ -114,5 +114,5 @@ def chat():
 #         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8080))
+    port = int(os.getenv('PORT', 8000))
     app.run(host='0.0.0.0', port=port, debug=False)
